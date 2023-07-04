47ee8ab87244235851733b42ae8b5e0d896c91f5
rec.fffixing <imdex1009@gmail.com>
2023-07-04 12:25:14 +0900

    modified

--- main.dart
+++ main.dart
@@ -1,6 +1,15 @@
 class Player {
-  final String name = 'AX';
-  int xp = 1500;
+  final String name;
+  int xp;
+  String team;
+  int age;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
 
   void sayHello() {
     print("Hi, my name is ${name}");
@@ -8,6 +17,13 @@ class Player {
 }
 
 void main() {
-  var player = Player();
+  var player = Player(
+    name: 'AX',
+    xp: 1200,
+    age: 12,
+    team: 'red',
+  );
   player.sayHello();
+  // var player2 = Player('Else', 3000, 'blue', 30);
+  // player2.sayHello();
 }
